51c679b53876315763c1229f9cd185b41a80cb91
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-11-07 11:56:45 +0100

    Update i18n.js

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,43 +1,50 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import english from './en.json';
-import french from "./fr.json";
-import * as RNLocalize from "react-native-localize";
+import french from './fr.json';
+import russian from './ru.json';
+
+import * as RNLocalize from 'react-native-localize';
 
 export const resources = {
     en: {
         translation: english,
     },
     fr: {
-        translation: french
-    }
-}
+        translation: french,
+    },
+    ru: {
+        translation: russian,
+    },
+};
 
-export const EN = { locale: 'en-US', language: 'English', code: 'en' }
-export const FR = { locale: 'fr-FR', language: 'Français', code: 'fr' }
+export const EN = { locale: 'en-US', language: 'English', code: 'en' };
+export const FR = { locale: 'fr-FR', language: 'Français', code: 'fr' };
+export const RU = { locale: 'ru-RU', language: 'Русский', code: 'ru' };
 
 export const languages = {
-  'en-US': EN,
-  'fr-FR': FR,
-}
+    'en-US': EN,
+    'fr-FR': FR,
+    'ru-RU': RU,
+};
 
-export const languageList = Object.values(languages)
+export const languageList = Object.values(languages);
 
 const languageDetector = {
     type: 'languageDetector',
     async: true,
     detect: (callback) => {
-        return callback(RNLocalize.getLocales()[0].languageCode)
+        return callback(RNLocalize.getLocales()[0].languageCode);
     },
-    init: () => { },
-    cacheUserLanguage: () => { },
-}
+    init: () => {},
+    cacheUserLanguage: () => {},
+};
 
 i18n
     .use(languageDetector)
     .use(initReactI18next)
     .init({
         resources,
-        compatibilityJSON: "v3",
-        fallbackLng: 'en'
-    })
+        compatibilityJSON: 'v3',
+        fallbackLng: 'en',
+    });
