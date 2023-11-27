42b2d1c47cff755326efce204baf4e82f2ea7fff
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-11-27 16:35:36 +0100

    Upgrade to React Native 0.72.7

--- ios/trenderappTests/trenderappTests.m
+++ ios/trenderappTests/trenderappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface trenderappTests : XCTestCase
+@interface TrenderTests : XCTestCase
 
 @end
 
-@implementation trenderappTests
+@implementation TrenderTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
@@ -26,7 +26,7 @@
   return NO;
 }
 
-- (void)testrenderappsWelcomeScreen
+- (void)testRendersWelcomeScreen
 {
   UIViewController *vc = [[[RCTSharedApplication() delegate] window] rootViewController];
   NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: [
-      'ts',
-      'tsx',
-      'js',
-      'jsx',
-      'json',
-      'node',
-    ],
+  preset: 'react-native',
 };
